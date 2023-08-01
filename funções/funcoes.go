f764b5fb45f90df99289c77a10a4c4111aaab741
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2023-08-01 15:04:52 -0300

    feat: functions & data

--- funções/funcoes.go
+++ funções/funcoes.go
@@ -6,6 +6,12 @@ func somar(n1 int8, n2 int8) int8 {
 	return n1 + n2
 }
 
+func calculosMatematicos(n1, n2 int8) (int8, int8)  {
+	soma := n1 + n2
+	subtracao := n1 + n2
+	return soma, subtracao
+}
+
 func main() {
 	somar := somar(10, 20)
 	fmt.Println(somar)
